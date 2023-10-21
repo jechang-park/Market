f385153204468027441213875fcdc3f5e1bb50d5
jechang-park <wpckddl1124@naver.com>
2023-10-21 17:22:45 +0900

    2

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -4,6 +4,8 @@ import productData from '../../data/data.json';
 import Header from '/components/Header';
 import Image from 'next/image';
 import { loadTossPayments } from "@tosspayments/payment-sdk";
+import React, { useState } from 'react';
+
 
 export default function ProductDetailPage() {
     const router = useRouter();
@@ -22,17 +24,46 @@ export default function ProductDetailPage() {
         });
     };
 
+
+    const [quantity, setQuantity] = useState(1);
+    const [totalAmount, setTotalAmount] = useState(product ? product.price : 0); // 기본 총 금액은 상품 가격으로 설정합니다.
+
+
+    const increaseQuantity = () => {
+        const newQuantity = quantity + 1;
+        setQuantity(newQuantity);
+        setTotalAmount(product.price * newQuantity);
+    };
+
+    const decreaseQuantity = () => {
+        if (quantity > 1) { // 수량이 1보다 클 때만 감소시킵니다.
+            const newQuantity = quantity - 1;
+            setQuantity(newQuantity);
+            setTotalAmount(product.price * newQuantity);
+        }
+    };
+
     if (!product) return <div>Loading...</div>;
 
     return (
         <div className='App'>
             <Header />
-            <div>
-                <Image src={product.src} width={200} height={200} style={{ borderRadius: "30px" }} />
-                <p>{product.title}</p>
-                <p>{product.description}</p>
-                <p>{product.price}원</p>
-                <button onClick={handleClick}>바로구매</button>
+            <div className='ctn_box'>
+                <div className='img'>
+                    <Image src={product.src} width={200} height={200} style={{ borderRadius: "30px" }} />
+                </div>
+                <div className='detail'>
+                    <p className='text_0'>{product.title}</p>
+                    <p className='text_1'>{product.price}원</p>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <button onClick={decreaseQuantity} style={{ marginRight: '10px' }}>-</button>
+                        <span>{quantity}</span> 
+                        <button onClick={increaseQuantity} style={{ marginLeft: '10px' }}>+</button>
+                    </div>
+                    <button onClick={handleClick}>바로구매</button>
+                    <button>장바구니</button>
+                    <p className='text_1'>총 금액: {totalAmount}원</p>
+                </div>
             </div>
         </div>
     );
