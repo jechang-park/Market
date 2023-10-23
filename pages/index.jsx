2fdbd1247e8d28735330f08625ac231d81695c58
jechang-park <wpckddl1124@naver.com>
2023-10-23 15:38:36 +0900

    2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import Header from '/components/Header';
 import Image from 'next/image';
-import productData from '../data/data.json';
 import Link from 'next/link';
-export default function Page() {
 
+// 이 함수는 빌드 시에 실행되어, 페이지가 요청될 때 정적 파일로 제공됩니다.
+export async function getStaticProps() {
+  // 외부 데이터 소스나 API를 호출할 수 있습니다.
+  // 이 예시에서는 로컬 데이터를 사용하므로 직접 import 합니다.
+  const productData = require('../data/data.json');
 
+  // props 키로 객체를 반환하여 컴포넌트로 데이터를 전달합니다.
+  return { props: { productData } };
+}
 
+// productData는 페이지 컴포넌트의 props로 전달됩니다.
+export default function Page({ productData }) {
   return (
     <div className="App">
       <Header />
@@ -14,8 +22,8 @@ export default function Page() {
       </div>
       <div className="ctn_1">
         {productData.map((product) => (
-          <Link href={`/products/${product.id}`}>
-            <div className="prdImg" key={product.id}>
+          <Link href={`/products/${product.id}`} key={product.id}>
+            <div className="prdImg">
               <Image src={product.src} width={200} height={200} style={{ borderRadius: "30px" }} />
               <p>{product.title}</p>
               <p>{product.price}원</p>
@@ -23,7 +31,6 @@ export default function Page() {
           </Link>
         ))}
       </div>
-
     </div>
   );
 }
--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,17 +1,13 @@
 // pages/products/[id].js
 import { useRouter } from 'next/router';
-import productData from '../../data/data.json';
 import Header from '/components/Header';
 import Image from 'next/image';
 import { loadTossPayments } from "@tosspayments/payment-sdk";
 import React, { useState } from 'react';
 import { useCart } from '../../components/cartContext';
 
-export default function ProductDetailPage() {
-    const router = useRouter();
-    const { id } = router.query;
+export default function ProductDetailPage({product}) {  // props 객체를 구조 분해 할당하여 product를 직접 가져옵니다.
     const { cartCount, addToCart } = useCart();
-    const product = productData.find(p => p.id == id);
     const [quantity, setQuantity] = useState(0);
     const [totalAmount, setTotalAmount] = useState(0);
 
@@ -48,7 +44,7 @@ export default function ProductDetailPage() {
         }
     };
 
-    if (!product) return <div>Loading...</div>;
+    console.log("props.product",product);
 
     return (
         <div className='App'>
@@ -72,4 +68,26 @@ export default function ProductDetailPage() {
             </div>
         </div>
     );
+}
+
+export async function getServerSideProps(context) {
+    const { params } = context;
+    const { id } = params;
+
+  // id를 정수로 변환합니다.
+  const numericId = parseInt(id, 10);
+
+
+    // 여기서 실제 서버나, 외부 API로부터 데이터를 가져올 수 있습니다.
+    // 이 예제에서는 정적 데이터를 사용합니다.
+    const productData = require('../../data/data.json');
+    const product = productData.find(p => p.id === numericId);
+
+    // 찾지 못한 경우 404 페이지를 반환할 수 있습니다.
+    if (!product) {
+        return { notFound: true };
+    }
+
+    // 페이지 컴포넌트로 props를 전달합니다.
+    return { props: { product } };
 }
