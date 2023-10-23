493550f0347b4ac633c902badcb04525fcd04b97
jechang-park <wpckddl1124@naver.com>
2023-10-23 16:49:19 +0900

    2

--- src/app/payments/complete/page.jsx
+++ src/app/payments/complete/page.jsx
@@ -1,36 +1,59 @@
-import Header from '/components/Header'; 
+import Header from '/components/Header';
 
 export default async function Page({ searchParams }) {
-  const secretKey = process.env.TOSS_SECRET_KEY || "";
-  const basicToken = Buffer.from(`${secretKey}:`, `utf-8`).toString("base64");
+  try {
+    const secretKey = process.env.TOSS_SECRET_KEY || "";
+    const basicToken = Buffer.from(`${secretKey}:`, `utf-8`).toString("base64");
 
-  const url = `https://api.tosspayments.com/v1/payments/orders/${searchParams.orderId}`;
-  const payments = await fetch(url, {
-    headers: {
-      Authorization: `Basic ${basicToken}`,
-      "Content-Type": "application/json",
-    },
-  }).then((res) => res.json());
+    const url = `https://api.tosspayments.com/v1/payments/orders/${searchParams.orderId}`;
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Basic ${basicToken}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-  const { card } = payments;
-  return (
-    <div className="app">
-      <Header></Header>
-      <div>
-      <h1>결제가 완료되었습니다</h1>
-      <ul>
-        <li>결제 상품 {payments.orderName}</li>
-        <li>주문번호 {payments.orderId} </li>
-        <li>카드회사 {payments.company}</li>
-        {/* <li>카드번호 {card.number}</li> */}
-        <li>결제금액 {card.amount}</li>
-        <li>
-          결제승인날짜{" "}
-          {Intl.DateTimeFormat().format(new Date(payments.approvedAt))}
-        </li>
-      </ul>
-    </div>
-    </div>
+    if (!response.ok) {
+      throw new Error('API 요청이 실패했습니다.');
+    }
 
-  );
+    const payments = await response.json();
+
+    if (!payments || !payments.card) {
+      throw new Error('결제 정보가 없습니다.');
+    }
+
+    const { card } = payments;
+
+    return (
+      <div className="app">
+        <Header></Header>
+        <div>
+          <h1>결제가 완료되었습니다</h1>
+          <ul>
+            <li>결제 상품 {payments.orderName}</li>
+            <li>주문번호 {payments.orderId} </li>
+            <li>카드회사 {payments.company}</li>
+            <li>카드번호 {card.number}</li>
+            <li>결제금액 {card.amount}</li>
+            <li>
+              결제승인날짜{" "}
+              {Intl.DateTimeFormat().format(new Date(payments.approvedAt))}
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    // 에러 처리를 여기에서 수행하고 사용자에게 에러 메시지를 표시합니다.
+    return (
+      <div className="app">
+        <Header></Header>
+        <div>
+          <h1>에러 발생</h1>
+          <p>{error.message}</p>
+        </div>
+      </div>
+    );
+  }
 }
